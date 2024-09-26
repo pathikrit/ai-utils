b63f1b0d3e72cdb38ccb03483ab1bf7a625ecf34
pathikrit <pathikritbhowmick@msn.com>
2024-09-26 12:01:42 -0400

    parseHtml util

--- index.js
+++ index.js
@@ -45,66 +45,6 @@ const askAi = (prompt) => llm.generateContent(dedent(prompt))
         }
     })
 
-const summarize = (req) => {
-    const url = req.query.url
-    const parsed = req.body ? extractFromHtml(req.body, url) : extract(url, {}, config.browser)
-    return parsed
-        .then(res => Object.assign(res, {text: convert(res.content)}))
-        .then(parsed => askAi(`
-            I have extracted the following information from this site:
-            url: ${parsed.url},
-            title: ${parsed.title},
-            description ${parsed.description}
-            content: ${parsed.text}
-
-            Generate a short title and summarize the above content and respond using the following JSON schema:
-            Return: {'title': string, 'summary': string}
-
-            where:
-            title: A short title for this content (max 3 or 4 words)
-
-            summary: a short Markdown note with relevant sections, sub-sections - each with bulleted and numbered lists and sub-lists.
-            The more structured the document is, the better.
-            But, be sure to be very short and succint for each bulleted item.
-            Feel free to include citations or links to products and resources as inline hyperlinks in Markdown.
-            Also, feel free to tabulate in markdown if needed.
-            Ignore disclaimers, self-promotions, acknowledgements etc.
-        `))
-        .then(({title, summary}) => `# [${title ?? parsed.title ?? parsed.description ?? parsed.url}](${url})\n\n${summary.replace(/\\n/g, '\n').replace(/\\t/g, '\t').replace(/\\"/g, '"')}`)
-        .then(md => res => res.send(md2html.makeHtml(md)))
-}
-
-const calendarize = (req) => {
-    const url = req.query.url
-    const parsed = req.body ? extractFromHtml(req.body, url) : extract(url, {}, config.browser)
-    return parsed
-        .then(res => Object.assign(res, {text: convert(res.content)}))
-        .then(parsed => askAi(`
-            I have extracted the following information from this site:
-            url: ${parsed.url},
-            title: ${parsed.title},
-            description ${parsed.description}
-            content: ${parsed.text}
-
-            Generate a calendar invite with given title, start date and time, end date and time, location and description and respond using the following JSON schema:
-            Return: {'title': string, 'start': string, 'end': string, 'location': string, 'details': string}
-
-            where:
-            title: Event title
-            start: Event start time in ISO format
-            end: Event end time in ISO format
-            location: Event location
-            details: Event description (short)
-        `))
-        .then(arg => {
-            const dateFormat = (d) => d.replaceAll('-', '').replaceAll(':', '').replaceAll('Z', '')
-            arg.details = [arg.details ?? parsed.description, url].join('\n\n')
-            arg.gcal = encodeURI(`https://calendar.google.com/calendar/render?action=TEMPLATE&text=${arg.title}&dates=${dateFormat(arg.start)}/${dateFormat(arg.end)}&location=${arg.location ?? ''}&details=${arg.details ?? ''}`)
-            console.log(arg)
-            return res => res.redirect(arg.gcal)
-        })
-}
-
 const responseCache = new Map()
 const immediateReturn = (handler) => (req, res) => {
     console.log(`${handler.name}: ${req.method} ${req.url} ...`)
@@ -114,6 +54,59 @@ const immediateReturn = (handler) => (req, res) => {
     setTimeout(() => responseCache.delete(requestId), config.responseTtl)
     return res.status(StatusCodes.ACCEPTED).send({id: requestId, resultUrl: `${req.protocol}://${req.get('host')}/result/${requestId}`})
 }
+
+const parseHtml = (req) => (req.body ? extractFromHtml(req.body, req.query.url) : extract(req.query.url, {}, config.browser))
+    .then(res => Object.assign(res, {text: convert(res.content)}))
+
+const summarize = (req) => parseHtml(req)
+    .then(parsed => askAi(`
+        I have extracted the following information from this site:
+        url: ${parsed.url},
+        title: ${parsed.title},
+        description ${parsed.description}
+        content: ${parsed.text}
+
+        Generate a short title and summarize the above content and respond using the following JSON schema:
+        Return: {'title': string, 'summary': string}
+
+        where:
+        title: A short title for this content (max 3 or 4 words)
+
+        summary: a short Markdown note with relevant sections, sub-sections - each with bulleted and numbered lists and sub-lists.
+        The more structured the document is, the better.
+        But, be sure to be very short and succint for each bulleted item.
+        Feel free to include citations or links to products and resources as inline hyperlinks in Markdown.
+        Also, feel free to tabulate in markdown if needed.
+        Ignore disclaimers, self-promotions, acknowledgements etc.
+    `))
+    .then(({title, summary}) => `# [${title ?? parsed.title ?? parsed.description ?? 'Original Article'}](${req.query.url})\n\n${summary.replace(/\\n/g, '\n').replace(/\\t/g, '\t').replace(/\\"/g, '"')}`)
+    .then(md => res => res.send(md2html.makeHtml(md)))
+
+const calendarize = (req) => parseHtml(req)
+    .then(parsed => askAi(`
+        I have extracted the following information from this site:
+        url: ${parsed.url},
+        title: ${parsed.title},
+        description ${parsed.description}
+        content: ${parsed.text}
+
+        Generate a calendar invite with given title, start date and time, end date and time, location and description and respond using the following JSON schema:
+        Return: {'title': string, 'start': string, 'end': string, 'location': string, 'details': string}
+
+        where:
+        title: Event title
+        start: Event start time in ISO format
+        end: Event end time in ISO format
+        location: Event location
+        details: Event description (short)
+    `))
+    .then(arg => {
+        const dateFormat = (d) => d.replaceAll('-', '').replaceAll(':', '').replaceAll('Z', '')
+        arg.details = [arg.details ?? parsed.description, req.query.url].join('\n\n')
+        arg.gcal = encodeURI(`https://calendar.google.com/calendar/render?action=TEMPLATE&text=${arg.title}&dates=${dateFormat(arg.start)}/${dateFormat(arg.end)}&location=${arg.location ?? ''}&details=${arg.details ?? ''}`)
+        console.log(arg)
+        return res => res.redirect(arg.gcal)
+    })
 
 express()
     .get('/', (req, res) => res.send('Try /summarize?url=$url or /calendarize?url=$url'))
